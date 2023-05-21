2f70bf89f4d2db21aa840af7ba79efd9152d2855
Sajith Ali S <119950449+Sajith-Ali-S@users.noreply.github.com>
2023-05-21 18:24:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+Person.__proto__.greet = function(){
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+}
+Employee.__proto__.greet = greet;
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+this.name=name;
+this.age=age;
+}
+Employee.__proto__.jobGreet = function(){
 
-function Employee(name, age, jobTitle) {}
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+}
+
+function Employee(name, age, jobTitle) {
+    this.name=name;
+    this.age=age;
+    this.jobTitle=jobTitle;
+}
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
